756b600db01da8d3e1d82edc52563073c39f8eb5
koy <kosatun@aol.com>
2019-04-26 15:23:45 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const express = require("express");
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+const db = require('./config/keys').mongoURI;
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log("Connected to MongoDB successfully"))
+  .catch(err => console.log(err));
+
+app.get("/", (req, res) => res.send("what assß"));
+
+const port = process.env.PORT || 5000; 
+
+app.listen(port, () => console.log(`Server is running on port ${port}`))
+
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
